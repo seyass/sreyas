cedc1e19a5c3ae1e3a049bddc6d5a8e96be0c335
Sreyas Namboothiri S <148409184+seyass@users.noreply.github.com>
2023-11-24 05:39:59 +0530

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,26 +1,27 @@
-$(document).ready(function(){
-  $("#submit-form").validate({
-    rules:{
-      fname:{
-        required:true
-        
-      },
-      gemail:{
-        required:true,
-        email:true
-      },
-      subject:{
-        required:true
-      },
-      message:{
-        required:true
+$('form').submit(function(event) {
+      // Check if the form is valid
+      if ($("#submit-form").valid()) {
+        $("#submit-form").submit((e)=>{
+          e.preventDefault()
+          $.ajax({
+              url:"https://script.google.com/macros/s/AKfycbyIgZwkIBgktnE4iOhM_KkNkyXXwnemGvODJ-nT6FJdr4pbp4b7kfBKFGD31pfjUxGYjA/exec",
+              data:$("#submit-form").serialize(),
+              method:"post",
+              success:function (response){
+                  alert("Form submitted successfully")
+                  window.location.reload()
+                  //window.location.href="https://google.com"
+              },
+              error:function (err){
+                  alert("Something Error")
+  
+              }
+          })
+      })
+          // Form is valid, continue with submission
+      } else {
+          // Form is not valid, prevent submission
+          event.preventDefault();
       }
-
-    },
-
-    messages:{
-      fname:"Enter your name"
-    }
-    
-  })
-})
+  });
+});
